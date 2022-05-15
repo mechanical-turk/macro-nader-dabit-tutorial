9f4b48828b692ba280bbc03871dca94161e87f96
Kerem Kazan <kerem.kazan@gmail.com>
2022-05-15 18:34:58 -0400

    mock token deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,6 +24,10 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/your-project-id",
+      accounts: [`0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`]
     }
   }
 };
